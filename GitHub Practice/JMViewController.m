df5c024642bef0ef347d1eb53f6d76960c370975
jonmariano21 <jonmariano21@gmail.com>
2014-06-19 13:40:10 -0700

    just created project

--- GitHub Practice/JMViewController.m
+++ GitHub Practice/JMViewController.m
@@ -18,6 +18,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"Just created GitHub Practice project");
 }
 
 - (void)didReceiveMemoryWarning
